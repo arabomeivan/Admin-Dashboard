c533f811a736c937664f496974a61a3669f439e3
arabomeivan <arabomeivan@protonmail.com>
2022-09-19 23:41:00 +0100

    Finished sidebar contents

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -10,7 +10,7 @@
         <div class="row logo">
 
           <div class="col-md-6">
-             <img src="../assets/Group2.jpg" alt="not loading" class="img-fluid">            
+             <img src="../assets/Logo.png" alt="not loading" class="img-fluid">            
           </div>
 
           <div class="col-md-6 dash">
@@ -18,6 +18,82 @@
              </div>
 
         </div>
+
+
+      <!--Main Menu text-->
+      <div class="row mainmenu">
+        <div class="col-md-12">
+          MAINMENU
+        </div>
+      </div>
+
+      
+      <!--Main Menu Tabs-->
+  <ul class="nav nav-tabs flex-column">
+
+    <!--Tab for dashboard-->
+  <li class="nav-item">
+    <a class="nav-link" href="#"><i class="fa-solid fa-chart-simple"></i>&nbsp;&nbsp;Dashboard</a>
+  </li>
+
+<!--Inbox-->
+  <li class="nav-item">
+    <a class="nav-link" href="#"><i class="fa-sharp fa-solid fa-comment"></i>&nbsp;&nbsp;View Requests</a>
+  </li>
+</ul>
+
+<!--Workspace text-->
+<div class="row workspace">
+        <div class="col-md-12">
+        Workspace
+        </div>
+      </div>
+
+
+<!--Workspace Tabs-->
+<ul class="nav nav-tabs flex-column">
+
+<!--add user-->
+<li class="nav-item">
+<a class="nav-link" href="#"><i class="fa-solid fa-user-plus"></i>&nbsp;&nbsp;Create User</a>
+</li>
+
+<!--add equipment-->
+<li class="nav-item">
+<a class="nav-link" href="#"><i class="fa-solid fa-desktop"></i>&nbsp;&nbsp;Add Equipment</a>
+</li>
+
+<!--view users-->
+<li class="nav-item">
+<a class="nav-link" href="#"><i class="fa-solid fa-user"></i>&nbsp;&nbsp;View Users</a>
+</li>
+
+<!--view inventory-->
+<li class="nav-item">
+<a class="nav-link" href="#"><i class="fa-solid fa-truck-ramp-box"></i>&nbsp;&nbsp;View Inventory</a>
+</li>
+</ul>
+
+
+
+<!--Need Help With Dash-->
+<div class="row help">
+
+   <!--help image-->
+  <div class="row">
+    <div class="col-md-12 help-circle-background rounded-circle">
+      <img src="../assets/bx_bxs-help-circle.png" alt="image not loading" class="img-fluid help-circle">
+    </div>
+  </div>
+ 
+  
+
+  <!--blurry background-->
+  <div class="col-md-12 needhelpbackground">
+    <!--Go to help Center Button-->
+    <button type="button" class="btn btn-primary gotohelp">Go to help center</button>
+  </div>
+</div>
 
         <!--End of Side bar content-->
     </div>
@@ -50,5 +126,115 @@ line-height: 29px;
 
 color: #FFFFFF;
 
+}
+
+.mainmenu
+{
+width: 95px;
+height: auto;
+margin-left: 30px;
+margin-top: 61px;
+
+font-family: 'Gilroy';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+
+color: #51CBFF;
+}
+
+.workspace
+{
+  width: 95px;
+height: auto;
+margin-left: 30px;
+margin-top: 32px;
+
+font-family: 'Gilroy';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+
+color: #51CBFF;
+}
+.nav-item
+{
+  width: 200px;
+  height: 56px;
+  margin-left: 20px;
+  font-family: 'Gilroy';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 21px;
+
+color: #FFFFFF;
+
+}
+
+.nav-link
+{
+   border: none;
+}
+.nav-link:hover
+{
+   color: white;
+}
+
+.nav-link:focus
+{
+  background-color: #1B2B65;
+  color: white;
+}
+.nav-tabs
+{
+  margin-top: 9px;
+   border-bottom-style: none;
+}
+.help
+{
+width: 204px;
+height: 100px;
+margin-left: auto;
+margin-right: auto;
+margin-top: 0px;
+}
+.needhelpbackground
+{
+  height: 70px;
+}
+.help-circle-background
+{
+width: 53px;
+height: 50px;
+margin-left: 72px;
+top: 40px;
+background: #1B2B65;
+}
+.help-circle
+{
+  margin-left: auto;
+  margin-right:auto;
+  margin-top: 10px;
+}
+.gotohelp
+{
+  background: #017EFA;
+  border-radius: 6px;
+   margin-left: auto;
+    margin-right: auto;
+    display: block;
+    margin-top: 30px;
+}
+.gotohelp:focus
+{
+  box-shadow: none;
+}
+.needhelpbackground
+{
+  background: #1B2B65;
+  border-radius: 8px;
 }
 </style>
